867d52253bc8226a673ca662a684f6bedb5b2c19
Jay Lee <jaylee@reed.edu>
2019-03-17 22:04:25 -0700

    Change turnout calculation, update linear model selection

--- code/precinct_math.R
+++ code/precinct_math.R
@@ -22,13 +22,14 @@ sf_wide <- readable(sf)
 sf_no_vote <- sf %>%
   filter(contest == "Mayor") %>%
   group_by(pref_voter_id) %>%
+  filter(sum(is.na(candidate)) < 3) %>%
   summarize(precinct = unique(precinct),
             over = sum(as.numeric(over_vote)) > 0,
             under = sum(as.numeric(under_vote)) > 0)
 
 # some basic results
 
-mean(sf_no_vote$over) # 0.4% of mayoral voters overvoted, which is (as expected) small
+mean(sf_no_vote$over) # 0.3% of mayoral voters overvoted, which is (as expected) small
 mean(sf_no_vote$under) # 25% of mayoral voters undervoted, which is pretty large (compare this to other SF elections...?)
 
 by_precinct <-  sf_no_vote %>%
@@ -49,10 +50,11 @@ sf_unadjusted <- sf_precincts
 # so we're gonna assume that proportions are equivalent on each half and the turnout count gets split
 # ACTUALLY that was a bad assumption because some are super unbalanced
 # we're gonna assume the pop split is the same as in 2010 and weight that way
+count_cols <- c('over_count', 'no_over_count', 'under_count', 'no_under_count')
 double_cases <- filter(sf_precincts, str_detect(precinct, '/')) %>%
   group_by(precinct) %>%
-  mutate(weight = population / sum(population))
-count_cols <- c('over_count', 'no_over_count', 'under_count', 'no_under_count')
+  mutate(weight = population / sum(population)) %>%
+  mutate_at(count_cols, round)
 sf_precincts[str_detect(sf_precincts$precinct, '/') %in% TRUE, count_cols] <- round(sf_precincts[str_detect(sf_precincts$precinct, '/') %in% TRUE, count_cols] * double_cases$weight)
 # this gives much nicer results
 
@@ -61,9 +63,10 @@ sf_precincts <- mutate(sf_precincts,
                        overvote_rate = over_count / (over_count + no_over_count),
                        undervote_rate = under_count / (under_count + no_under_count),
                        turnout = under_count + no_under_count,
-                       no_turnout = population - turnout,
+                       no_turnout = round(population) - turnout,
                        turnout_rate = turnout / population) %>%
-  na.omit()
+  na.omit() %>%
+  filter(turnout_rate < 1)
 
 sf_wrong <- left_join(sf_precincts, demo_data_int, by = c('PREC_2017' = 'PREC_2017_wrong'))
 
@@ -77,7 +80,7 @@ nvars <- 16
 
 iters <- 100
 turnout_models <- vector("list", length = iters)
-turnout_bic <- vector(iters)
+turnout_bic <- vector(length = iters)
 
 for (i in seq_len(iters)) {
   train_index <- sample(c(TRUE, FALSE), size = nrow(sf_precincts), replace = TRUE)
@@ -102,64 +105,67 @@ best_linear_turnout <- lm(turnout_rate ~ pop_18_24 + pop_45_64 + college, data =
 
 
 
-# linear model for overvoting #####
-# bootstrap these instead? to get different splits
+# linear overvoting #####
+
 over_formula <- overvote_rate ~ female + pop_18_24 + pop_25_44 + pop_45_64 +
   pop_65_up + hispanic + white + black + native + asian + pac_islander +
   other_race + no_hs + college + poverty + no_english
 nvars <- 16
 
-linear_over_backward <- regsubsets(over_formula, data = training, method = 'backward', nvmax = nvars)
-linear_over_forward <- regsubsets(over_formula, data = training, method = 'forward', nvmax = nvars)
-# backward selection
-test.mat <- model.matrix(over_formula, data = test)
-back_errors <- rep(NA, nvars)
-for (i in 1:nvars) {
-  coefi <- coef(linear_over_backward, id = i)
-  pred <- test.mat[ ,names(coefi)]%*%coefi
-  back_errors[i] <- mean((test$overvote_rate - pred)^2)
-}
-coef(linear_over_backward, which.min(back_errors))
-# forward selection
-test.mat <- model.matrix(over_formula, data = test)
-for_errors <- rep(NA, nvars)
-for (i in 1:nvars) {
-  coefi <- coef(linear_over_forward, id = i)
-  pred <- test.mat[ ,names(coefi)]%*%coefi
-  for_errors[i] <- mean((test$overvote_rate - pred)^2)
-}
-coef(linear_over_forward, which.min(for_errors))
-# they agree here!
-best_linear_over <- lm(overvote_rate ~ hispanic + black + no_english, data = sf_precincts)
+iters <- 100
+over_models <- vector("list", length = iters)
+over_bic <- vector(length = iters)
 
-# linear model for undervoting #####
+for (i in seq_len(iters)) {
+  train_index <- sample(c(TRUE, FALSE), size = nrow(sf_precincts), replace = TRUE)
+  training <- filter(sf_precincts, train_index)
+  test <- filter(sf_precincts, !train_index)
+  
+  linear_over <- regsubsets(over_formula, data = training, method = 'exhaustive', nvmax = nvars)
+  # best subset selection
+  over_models[[i]] <- coef(linear_over, which.min(summary(linear_over)$bic))
+  over_bic[i] <- min(summary(linear_over)$bic)
+}
+
+over_models[order(over_bic)]
+
+# so this isn't a very mathematical heuristic 
+# but the best few models consistently have
+# black, college
+
+best_linear_over <- lm(overvote_rate ~ black + college, data = sf_precincts)
+
+
+
+# linear undervoting #####
+
 under_formula <- undervote_rate ~ female + pop_18_24 + pop_25_44 + pop_45_64 +
   pop_65_up + hispanic + white + black + native + asian + pac_islander +
   other_race + no_hs + college + poverty + no_english
+nvars <- 16
 
-linear_under_backward <- regsubsets(under_formula, data = training, method = 'backward', nvmax = nvars)
-linear_under_forward <- regsubsets(under_formula, data = training, method = 'forward', nvmax = nvars)
-# backward selection
-test.mat <- model.matrix(under_formula, data = test)
-back_errors <- rep(NA, nvars)
-for (i in 1:nvars) {
-  coefi <- coef(linear_under_backward, id = i)
-  pred <- test.mat[ ,names(coefi)]%*%coefi
-  back_errors[i] <- mean((test$undervote_rate - pred)^2)
+iters <- 100
+under_models <- vector("list", length = iters)
+under_bic <- vector(length = iters)
+
+for (i in seq_len(iters)) {
+  train_index <- sample(c(TRUE, FALSE), size = nrow(sf_precincts), replace = TRUE)
+  training <- filter(sf_precincts, train_index)
+  test <- filter(sf_precincts, !train_index)
+  
+  linear_under <- regsubsets(over_formula, data = training, method = 'exhaustive', nvmax = nvars)
+  # best subset selection
+  under_models[[i]] <- coef(linear_under, which.min(summary(linear_under)$bic))
+  under_bic[i] <- min(summary(linear_under)$bic)
 }
-coef(linear_under_backward, which.min(back_errors))
-# forward selection
-test.mat <- model.matrix(under_formula, data = test)
-for_errors <- rep(NA, nvars)
-for (i in 1:nvars) {
-  coefi <- coef(linear_under_forward, id = i)
-  pred <- test.mat[ ,names(coefi)]%*%coefi
-  for_errors[i] <- mean((test$undervote_rate - pred)^2)
-}
-coef(linear_under_forward, which.min(for_errors))
-# they do not agree here! backwards has a lower test MSE
-best_linear_under <- lm(overvote_rate ~ pop_18_24 + pop_25_44 + pop_45_64 +
-                          white + black + no_hs + college, data = sf_precincts)
+
+under_models[order(under_bic)]
+
+# so this isn't a very mathematical heuristic 
+# but the best few models consistently have
+# black, no_hs
+
+best_linear_under <- lm(undervote_rate ~ black + no_hs, data = sf_precincts)
 
 
 # logistic regressions

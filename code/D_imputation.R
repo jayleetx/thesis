8987dfb7b1d4bb007f7a50a3ee0d64b6b13d91c3
Jay Lee <jaylee@reed.edu>
2019-04-27 16:17:45 -0700

    Honestly this is so many things...

--- code/D_imputation.R
+++ code/D_imputation.R
@@ -5,14 +5,20 @@ library(dplyr)
 library(tidyr)
 library(rcv)
 library(simputation)
-library(VIM)
+library(ggplot2)
+library(sf)
+library(nnet)
 options(simputation.hdbackend="VIM")
+its <- 10
 
 load(here('data', 'sf_ballot.RData'))
 
 orig_wide <- readable(sf) %>% filter(contest == 'Mayor')
 
 precincts <- distinct(sf, pref_voter_id, precinct)
+
+imputation_counts <- data.frame()
+imputation_results <- data.frame()
 
 # so this just organizes the data so that duplicates are dropped and non-NAs moved to the front
 sf_wide <- sf %>%
@@ -29,13 +35,24 @@ sf_wide <- sf %>%
          x2 = `2`,
          x3 = `3`)
 
-orig_counts <- count(sf_wide, `x1`, `x2`, `x3`) %>%
+# originalcounts & results #####
+counts <- sf_wide %>%
+  rename(`1` = x1,
+         `2` = x2,
+         `3` = x3) %>%
+  count(`1`, `2`, `3`) %>%
   arrange(desc(n)) %>%
-  mutate(prop = n / sum(n))
-
-orig_results <- rcv_tally(sf, 'Mayor') %>%
-  mutate(impute = 'original',
+  mutate(prop = n / sum(n),
+         impute = 'original',
          trial = 1)
+
+results <- rcv_tally(sf, 'Mayor') %>%
+  mutate(impute = 'original',
+         trial = 1,
+         rank = row_number())
+
+imputation_counts <- bind_rows(imputation_counts, counts)
+imputation_results <- bind_rows(imputation_results, results)
 
 # listwise deletion #####
 listwise <- sf_wide %>%
@@ -44,25 +61,74 @@ listwise <- sf_wide %>%
          `2` = x2,
          `3` = x3)
 
-listwise_long <- listwise %>%
+long <- listwise %>%
   gather(key = 'vote_rank', value = 'candidate', `1`, `2`, `3`) %>%
   arrange(pref_voter_id)
 
-listwise_counts <- count(listwise, `1`, `2`, `3`) %>%
+counts <- count(listwise, `1`, `2`, `3`) %>%
   arrange(desc(n)) %>%
-  mutate(prop = n / sum(n))
-
-listwise_results <- rcv_tally(listwise_long) %>%
-  mutate(impute = 'listwise',
+  mutate(prop = n / sum(n),
+         impute = 'listwise',
          trial = 1)
 
+results <- rcv_tally(long) %>%
+  mutate(impute = 'listwise',
+         trial = 1,
+         rank = row_number())
+
+imputation_counts <- bind_rows(imputation_counts, counts)
+imputation_results <- bind_rows(imputation_results, results)
+
+# imputed on nothing - selection from a random row #####
+# this is gonna have a ton of weird combinations that don't show up in the data that often
+
+unique_cands <- na.omit(unique(sf$candidate[sf$contest == "Mayor"]))
+
+for (cycle in seq_len(its)) {
+  imputed <- sf_wide
+  for (i in unique_cands) {
+    missing_rows <- filter(imputed, is.na(x2) & (x1 == i))
+    donor_rows <- filter(imputed, !is.na(x2) & (x2 != i))
+    imputed$x2[is.na(imputed$x2) &
+                 (imputed$x1 == i)] <- sample(donor_rows$x2,
+                                              nrow(missing_rows))
+    
+    for (j in unique_cands[unique_cands != i]) {
+      missing_rows <- filter(imputed, is.na(x3) & (x1 == i) & (x2 == j))
+      donor_rows <- filter(imputed, !is.na(x3) & !(x3 %in% c(i,j)))
+      imputed$x3[is.na(imputed$x3) &
+                   (imputed$x1 == i) &
+                   (imputed$x2 == j)] <- sample(donor_rows$x2,
+                                                nrow(missing_rows)) 
+    }
+  }
+  
+  imputed_done <-  rename(imputed,
+                     `1` = x1,
+                     `2` = x2,
+                     `3` = x3)
+  
+  long <- gather(imputed_done, key = 'vote_rank', value = 'candidate', `1`, `2`, `3`) %>%
+    arrange(pref_voter_id)
+  
+  counts <- count(imputed_done, `1`, `2`, `3`) %>%
+    arrange(desc(n)) %>%
+    mutate(new_prop = n / sum(n)) %>%
+    mutate(impute = 'nothing',
+           trial = cycle)
+
+  results <- rcv_tally(long) %>%
+    mutate(impute = 'nothing',
+           trial = cycle,
+           rank = row_number())
+  
+  imputation_counts <- bind_rows(imputation_counts, counts)
+  imputation_results <- bind_rows(imputation_results, results)
+}
 
 # imputed on vote choice alone #####
 
-vc_counts <- data.frame()
-vc_results <- data.frame()
-
-for (i in seq_len(1000)){
+for (i in seq_len(its)){
   sf_wide_rand <- sample_frac(sf_wide)
   imputed <- impute_rhd(sf_wide, x2 ~ x1) %>%
     impute_rhd(x3 ~ x1 + x2) %>%
@@ -79,25 +145,27 @@ for (i in seq_len(1000)){
     mutate(new_prop = n / sum(n)) %>%
     mutate(impute = 'vote_choice',
            trial = i)
-  vc_counts <- bind_rows(vc_counts, counts)
-  
+
   results <- rcv_tally(long) %>%
     mutate(impute = 'vote_choice',
            trial = i,
            rank = row_number())
-  vc_results <- bind_rows(vc_results, results)
+  
+  imputation_counts <- bind_rows(imputation_counts, counts)
+  imputation_results <- bind_rows(imputation_results, results)
 }
-# ¸¸¸¸Çdwddddddddddd# so weird stuff
-# looping over it is giving zero variability in the metrics I'm interested in
-# they're literally identical
-# the imputation is being random, like we want
-# maybe it's just law-of-large-numbers-ing itself?
-# update I'm just dumb
 
+ extra <- imputation_results %>%
+   filter(impute == 'vote_choice') %>%
+   filter(candidate != 'NA') %>%
+   group_by(trial) %>%
+   mutate(prop = round1 / sum(round1, na.rm = TRUE)) %>%
+   slice(1:5)
 
-vc_diff <- full_join(listwise_counts, vc_counts, by = c('1', '2', '3')) %>%
-  select(-n.x, -n.y) %>%
-  mutate(difference = new_prop - prop)
+# hey what in the WORLD is going on when you iterate the rounds forward from 2 to 3
+ ggplot(extra, aes(x = prop)) + 
+   geom_histogram() + facet_wrap(~candidate, scales = 'free')
+
 
 # so the MOST that any group is off (from original listwise) by here is 1 percentage point
 # and almost all are within a fifth of a point
@@ -110,39 +178,135 @@ vc_diff <- full_join(listwise_counts, vc_counts, by = c('1', '2', '3')) %>%
 # OH and maybe see how when you impute multiple times the order / variability changes
 # simulating multiple times to get a "distribution" among different candidate placements
 
+# using MSE (or another case-wise metric) on each one to get an accuracy measure isn't great
+# because the methods we're using are so coarse, all we can hope for is distributional accuracy
+
 # another metric - London Breed's vote share?
 # and how does this decrease the percentage of votes that are exhausted
 # probably compare all of the methods to just regular
 
 # imputed on vote choice and precinct #####
-vc_pct <- impute_rhd(sf_wide, x2 ~ x1 + precinct) %>%
-  impute_rhd(x2 ~ x1) %>%
-  impute_rhd(x3 ~ x1 + x2 + precinct) %>%
-  impute_rhd(x3 ~ x1 + x2) %>%
-  impute_rhd(x3 ~ x1) %>%
-  rename(`1` = x1,
-         `2` = x2,
-         `3` = x3)
 
-# you have to use these fill-in steps to make sure everything is completed
-# an interesting thing - see how many cases are uncompleted by each of the methods
+for (i in seq_len(its)){
+  imputed <- impute_rhd(sf_wide, x2 ~ x1 + precinct) %>%
+    impute_rhd(x2 ~ x1) %>%
+    impute_rhd(x3 ~ x1 + x2 + precinct) %>%
+    impute_rhd(x3 ~ x1 + x2) %>%
+    impute_rhd(x3 ~ x1) %>%
+    rename(`1` = x1,
+           `2` = x2,
+           `3` = x3)
+  # you have to use these fill-in steps to make sure everything is completed
+  # an interesting thing - see how many cases are uncompleted by each of the methods
+  
+  long <- gather(imputed, key = 'vote_rank', value = 'candidate', `1`, `2`, `3`) %>%
+    arrange(pref_voter_id)
+  
+  counts <- count(imputed, `1`, `2`, `3`) %>%
+    arrange(desc(n)) %>%
+    mutate(new_prop = n / sum(n)) %>%
+    mutate(impute = 'vote_choice_precinct',
+           trial = i)
 
-vc_pct_long <- gather(vc_pct, key = 'vote_rank', value = 'candidate', `1`, `2`, `3`) %>%
-  arrange(pref_voter_id)
+  results <- rcv_tally(long) %>%
+    mutate(impute = 'vote_choice_precinct',
+           trial = i,
+           rank = row_number())
+  
+  imputation_counts <- bind_rows(imputation_counts, counts)
+  imputation_results <- bind_rows(imputation_results, results)
+}
 
-# use the VIM package to do some visualizations/explorations
+# multinomial model #####
 
-vc_pct_counts <- count(vc_pct, `1`, `2`, `3`) %>%
-  arrange(desc(n)) %>%
-  mutate(new_prop = n / sum(n))
+load(here('data', 'combined_precincts.RData'))
+whole_data <- left_join(sf_wide, combined_pcts, by = 'precinct')
 
-vc_pct_diff <- full_join(listwise_counts, vc_pct_counts, by = c('1', '2', '3')) %>%
-  select(-n.x, -n.y) %>%
-  mutate(difference = new_prop - prop)
+# doing this just based on first vote choice should be somewhat identical to hot deck
 
-hist(abs(vc_pct_diff$difference))
+# do this on demos plus vote choice
+# demos are:
+# race (asian and black, bc two of the major candidates were as such)
+# female
+# age (youngest and oldest, bc they vote the least and most respectively)
+# education, via % with no HS degree (or college, but only do one)
+# poverty
+# and not speaking english
 
-vc_pct_results <- rcv_tally(vc_pct_long)
+
+complete_12 <- filter(whole_data, !is.na(x2))
+complete_123 <- filter(whole_data, !is.na(x3))
+missing2 <- filter(whole_data, is.na(x2))
+# complete_123 is up here so the model for x3 isn't built on x2, mostly to save computing time
+# HOWEVER it is going to predict based on the imputed x2 later, to make the rows work out
+
+multi_model2 <- multinom(x2 ~ x1
+                       + female 
+                       + pop_18_24 
+                       + pop_65_plus 
+                       + black 
+                       + asian 
+                       + no_hs 
+                       + poverty 
+                       + no_english, data = complete_12, maxit = 200)
+# doing demographics alone can cause overlap, so we have to include first vote choice
+# I could get around it by figuring out how to exclude earlier cases, but no time...
+
+multi_model3 <- multinom(x3 ~ x1 + x2
+                         + female 
+                         + pop_18_24 
+                         + pop_65_plus 
+                         + black 
+                         + asian 
+                         + no_hs 
+                         + poverty 
+                         + no_english, data = complete_123, maxit = 200)
+
+for (i in seq_len(its)) {
+  imputed <- whole_data
+  
+  # round 2
+  probs <- predict(multi_model2, type = "probs", newdata = missing2) %>% data.frame()
+  
+  pred <- apply(probs, 1, sample, x=colnames(probs), size=1, replace = TRUE) %>%
+    stringr::str_replace_all('\\.', ' ') %>% # this puts spaces back in
+    stringr::str_replace_all('  ', '\\. ') %>% # this fixes middle initials
+    stringr::str_replace_all('WRITE IN', 'WRITE-IN') # this fixes write-ins
+  
+  imputed$x2[is.na(imputed$x2)] <- pred
+  
+  # round 3
+  missing3 <- filter(imputed, is.na(x3))
+  probs <- predict(multi_model3, type = "probs", newdata = missing3) %>% data.frame()
+  
+  pred <- apply(probs, 1, sample, x=colnames(probs), size=1, replace = TRUE) %>%
+    stringr::str_replace_all('\\.', ' ') %>%
+    stringr::str_replace_all('  ', '\\. ') %>%
+    stringr::str_replace_all('WRITE IN', 'WRITE-IN')
+  imputed$x3[is.na(imputed$x3)] <- pred
+  
+  imputed <- select(imputed, colnames(sf_wide)) %>%
+    rename(`1` = x1,
+           `2` = x2,
+           `3` = x3)
+  
+  long <- gather(imputed, key = 'vote_rank', value = 'candidate', `1`, `2`, `3`) %>%
+    arrange(pref_voter_id)
+  
+  counts <- count(imputed, `1`, `2`, `3`) %>%
+    arrange(desc(n)) %>%
+    mutate(new_prop = n / sum(n)) %>%
+    mutate(impute = 'multinomial',
+           trial = i)
+  
+  results <- rcv_tally(long) %>%
+    mutate(impute = 'multinomial',
+           trial = i,
+           rank = row_number())
+  
+  imputation_counts <- bind_rows(imputation_counts, counts)
+  imputation_results <- bind_rows(imputation_results, results)
+}
 
 # summary #####
 
@@ -150,6 +314,15 @@ vc_pct_results <- rcv_tally(vc_pct_long)
 # orig_results, listwise_results, imputed_results
 
 # I expected the by-precinct one to make more of a difference, bc some precincts are so under-represented
-pct_under <- sf_wide %>%
-  group_by(precinct) %>%
-  summarize(uv_rate = mean(is.na(x3)))
+if (!file.exists(here('data', 'imputation_counts.csv'))) {
+  readr::write_csv(imputation_counts, here('data', 'imputation_counts.csv'))
+} else {
+  readr::write_csv(imputation_counts, here('data', 'imputation_counts.csv'), append = TRUE)
+}
+
+if (!file.exists(here('data', 'imputation_results.csv'))) {
+  readr::write_csv(imputation_results, here('data', 'imputation_results.csv'))
+} else {
+  readr::write_csv(imputation_results, here('data', 'imputation_results.csv'), append = TRUE)
+}
+
